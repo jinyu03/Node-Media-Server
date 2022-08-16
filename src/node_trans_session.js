9bc692bce51266881bf72b41ec4db7e9f1216c11
wangzhao <wangzhao01@xinye.com>
2022-08-16 13:49:05 +0800

    修改mp4生成路径

--- src/node_trans_session.js
+++ src/node_trans_session.js
@@ -11,113 +11,104 @@ const dateFormat = require('dateformat');
 const mkdirp = require('mkdirp');
 const fs = require('fs');
 
-const isHlsFile = (filename) => filename.endsWith('.ts') || filename.endsWith('.m3u8')
-const isTemFiles = (filename) => filename.endsWith('.mpd') || filename.endsWith('.m4s') || filename.endsWith('.tmp')
-
 class NodeTransSession extends EventEmitter {
-  constructor(conf) {
-    super();
-    this.conf = conf;
-    this.getConfig = (key = null) => {
-      if (!key) return
-      if (typeof this.conf != 'object') return
-      if (this.conf.args && typeof this.conf.args === 'object' && this.conf.args[key]) return this.conf.args[key]
-      return this.conf[key]
+    constructor(conf) {
+        super();
+        this.conf = conf;
     }
-  }
 
-  run() {
-    let vc = this.conf.vc || 'copy';
-    let ac = this.conf.ac || 'copy';
-    let inPath = 'rtmp://127.0.0.1:' + this.conf.rtmpPort + this.conf.streamPath;
-    let ouPath = `${this.conf.mediaroot}/${this.conf.streamApp}/${this.conf.streamName}`;
-    let mapStr = '';
+    run() {
+        let vc = this.conf.vc || 'copy';
+        let ac = this.conf.ac || 'copy';
+        let inPath = 'rtmp://127.0.0.1:' + this.conf.rtmpPort + this.conf.streamPath;
+        // let ouPath = `${this.conf.mediaroot}/${this.conf.streamApp}/${this.conf.streamName}`;
+        let ouPath = `${this.conf.mediaroot}/${this.conf.streamApp}`;
+        let mapStr = '';
 
-    if (this.conf.rtmp && this.conf.rtmpApp) {
-      if (this.conf.rtmpApp === this.conf.streamApp) {
-        Logger.error('[Transmuxing RTMP] Cannot output to the same app.');
-      } else {
-        let rtmpOutput = `rtmp://127.0.0.1:${this.conf.rtmpPort}/${this.conf.rtmpApp}/${this.conf.streamName}`;
-        mapStr += `[f=flv]${rtmpOutput}|`;
-        Logger.log('[Transmuxing RTMP] ' + this.conf.streamPath + ' to ' + rtmpOutput);
-      }
+        if (this.conf.rtmp && this.conf.rtmpApp) {
+            if (this.conf.rtmpApp === this.conf.streamApp) {
+                Logger.error('[Transmuxing RTMP] Cannot output to the same app.');
+            } else {
+                let rtmpOutput = `rtmp://127.0.0.1:${this.conf.rtmpPort}/${this.conf.rtmpApp}/${this.conf.streamName}`;
+                mapStr += `[f=flv]${rtmpOutput}|`;
+                Logger.log('[Transmuxing RTMP] ' + this.conf.streamPath + ' to ' + rtmpOutput);
+            }
+        }
+        if (this.conf.mp4) {
+            this.conf.mp4Flags = this.conf.mp4Flags ? this.conf.mp4Flags : '';
+            //   let mp4FileName = dateFormat('yyyy-mm-dd-HH-MM-ss') + '666.mp4';
+            let mp4FileName = this.conf.streamName + '.mp4';
+            let mapMp4 = `${this.conf.mp4Flags}${ouPath}/${mp4FileName}`;
+            mapStr += mapMp4;
+            Logger.log(
+                '[Transmuxing MP4] ' + this.conf.streamPath + ' to ' + ouPath + '/' + mp4FileName
+            );
+        }
+        if (this.conf.hls) {
+            this.conf.hlsFlags = this.conf.hlsFlags ? this.conf.hlsFlags : '';
+            let hlsFileName = 'index.m3u8';
+            let mapHls = `${this.conf.hlsFlags}${ouPath}/${hlsFileName}|`;
+            mapStr += mapHls;
+            Logger.log(
+                '[Transmuxing HLS] ' + this.conf.streamPath + ' to ' + ouPath + '/' + hlsFileName
+            );
+        }
+        if (this.conf.dash) {
+            this.conf.dashFlags = this.conf.dashFlags ? this.conf.dashFlags : '';
+            let dashFileName = 'index.mpd';
+            let mapDash = `${this.conf.dashFlags}${ouPath}/${dashFileName}`;
+            mapStr += mapDash;
+            Logger.log(
+                '[Transmuxing DASH] ' + this.conf.streamPath + ' to ' + ouPath + '/' + dashFileName
+            );
+        }
+        mkdirp.sync(ouPath);
+        let argv = ['-y', '-i', inPath];
+        Array.prototype.push.apply(argv, ['-c:v', vc]);
+        Array.prototype.push.apply(argv, this.conf.vcParam);
+        Array.prototype.push.apply(argv, ['-c:a', ac]);
+        Array.prototype.push.apply(argv, this.conf.acParam);
+        Array.prototype.push.apply(argv, ['-f', 'tee', '-map', '0:a?', '-map', '0:v?', mapStr]);
+        argv = argv.filter(n => {
+            return n;
+        }); //去空
+        this.ffmpeg_exec = spawn(this.conf.ffmpeg, argv);
+        this.ffmpeg_exec.on('error', e => {
+            Logger.ffdebug(e);
+        });
+
+        this.ffmpeg_exec.stdout.on('data', data => {
+            Logger.ffdebug(`FF输出：${data}`);
+        });
+
+        this.ffmpeg_exec.stderr.on('data', data => {
+            Logger.ffdebug(`FF输出：${data}`);
+        });
+
+        this.ffmpeg_exec.on('close', code => {
+            Logger.log('[Transmuxing end] ' + this.conf.streamPath);
+            this.emit('end');
+            fs.readdir(ouPath, function (err, files) {
+                if (!err) {
+                    files.forEach(filename => {
+                        if (
+                            filename.endsWith('.ts') ||
+                            filename.endsWith('.m3u8') ||
+                            filename.endsWith('.mpd') ||
+                            filename.endsWith('.m4s') ||
+                            filename.endsWith('.tmp')
+                        ) {
+                            fs.unlinkSync(ouPath + '/' + filename);
+                        }
+                    });
+                }
+            });
+        });
     }
-    if (this.conf.mp4) {
-      this.conf.mp4Flags = this.conf.mp4Flags ? this.conf.mp4Flags : '';
-      let mp4FileName = dateFormat('yyyy-mm-dd-HH-MM-ss') + '.mp4';
-      let mapMp4 = `${this.conf.mp4Flags}${ouPath}/${mp4FileName}|`;
-      mapStr += mapMp4;
-      Logger.log('[Transmuxing MP4] ' + this.conf.streamPath + ' to ' + ouPath + '/' + mp4FileName);
+
+    end() {
+        this.ffmpeg_exec.kill();
     }
-    if (this.conf.hls) {
-      this.conf.hlsFlags = this.getConfig('hlsFlags') || '';
-      let hlsFileName = 'index.m3u8';
-      let mapHls = `${this.conf.hlsFlags}${ouPath}/${hlsFileName}|`;
-      mapStr += mapHls;
-      Logger.log('[Transmuxing HLS] ' + this.conf.streamPath + ' to ' + ouPath + '/' + hlsFileName);
-    }
-    if (this.conf.dash) {
-      this.conf.dashFlags = this.conf.dashFlags ? this.conf.dashFlags : '';
-      let dashFileName = 'index.mpd';
-      let mapDash = `${this.conf.dashFlags}${ouPath}/${dashFileName}`;
-      mapStr += mapDash;
-      Logger.log('[Transmuxing DASH] ' + this.conf.streamPath + ' to ' + ouPath + '/' + dashFileName);
-    }
-    mkdirp.sync(ouPath);
-    let argv = ['-y', '-i', inPath];
-    Array.prototype.push.apply(argv, ['-c:v', vc]);
-    Array.prototype.push.apply(argv, this.conf.vcParam);
-    Array.prototype.push.apply(argv, ['-c:a', ac]);
-    Array.prototype.push.apply(argv, this.conf.acParam);
-    Array.prototype.push.apply(argv, ['-f', 'tee', '-map', '0:a?', '-map', '0:v?', mapStr]);
-    argv = argv.filter((n) => { return n; }); //去空
-    
-    this.ffmpeg_exec = spawn(this.conf.ffmpeg, argv);
-    this.ffmpeg_exec.on('error', (e) => {
-      Logger.ffdebug(e);
-    });
-
-    this.ffmpeg_exec.stdout.on('data', (data) => {
-      Logger.ffdebug(`FF输出：${data}`);
-    });
-
-    this.ffmpeg_exec.stderr.on('data', (data) => {
-      Logger.ffdebug(`FF输出：${data}`);
-    });
-
-    this.ffmpeg_exec.on('close', (code) => {
-      Logger.log('[Transmuxing end] ' + this.conf.streamPath);
-      this.emit('end');
-      this.cleanTempFiles(ouPath)
-      this.deleteHlsFiles(ouPath)
-    });
-  }
-
-  end() {
-    this.ffmpeg_exec.kill();
-  }
-
-  // delete hls files
-  deleteHlsFiles (ouPath) {
-    if ((!ouPath && !this.conf.hls) || this.getConfig('hlsKeep')) return
-    fs.readdir(ouPath, function (err, files) {
-      if (err) return
-      files.filter((filename) => isHlsFile(filename)).forEach((filename) => {
-        fs.unlinkSync(`${ouPath}/${filename}`);
-      });
-    });
-  }
-
-  // delete the other files
-  cleanTempFiles (ouPath) {
-    if (!ouPath) return
-    fs.readdir(ouPath, function (err, files) {
-      if (err) return
-      files.filter((filename) => isTemFiles(filename)).forEach((filename) => {
-        fs.unlinkSync(`${ouPath}/${filename}`);
-      });
-    });
-  }
 }
 
 module.exports = NodeTransSession;
